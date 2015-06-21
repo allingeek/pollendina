e5ab52f2a5a4d760f68264be6ce3d2f28b60e673
Jason Huddleston <huddlesj@gmail.com>
2015-06-21 02:56:15 -0700

    updated init.sh
    Added bash code

--- service/src/init.sh
+++ service/src/init.sh
@@ -6,16 +6,20 @@
 #   index.txt
 #   serial.txt
 
+FILES=(openssl-ca.cnf cacert.pem cakey.pem index.txt serial.txt)
+SDIR="/pollendina"
+DDIR="/opt/pollendina"
 
-# Test existence of each of the require resources
+for i in ${FILES[@]}; do
+  # Test existence of each of the require resources
+  if [ ! -f $DDIR/$i ];then
+          cp $SDIR/$i $DDIR/$i
+  fi
+done
 
-# touch /opt/pollendina/index.txt
-
-# echo 01 >> /opt/pollendina/serial.txt
 
 # openssl req -x509 -config openssl-ca.cnf -newkey rsa:4096 -sha256 -nodes -out cacert.pem -outform PEM
 
-# cp /pollendina/openssl-ca.cnf /opt/pollendina/openssl-ca.cnf
 
 
 exec "$@" # run the default command
