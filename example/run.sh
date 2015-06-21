73730b5e0a648fa1beacf8e62eb0a57f74c571fd
Dario Nasicmento <dnascime@cisco.com>
2015-06-20 22:02:59 -0700

    script to launch container

---
+++ example/run.sh
@@ -0,0 +1,3 @@
+POLLENDINA_TOCKEN=$(openssl rand -base64 32)
+curl --cert "cacert.pem" --data "token=${POLLENDINA_TOCKEN}&cn=${COMMON_NAME}" https://$CA_IP/v1/authorization
+docker run -e POLLENDINA_TOCKEN="$POLLENDINA_TOCKEN" -e COMMON_NAME="$COMMON_NAME" -i -t dario/base /bin/bash
