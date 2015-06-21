e007a8209d7ecf1a5e5f16d30013aae7ae67f647
Dario Nasicmento <dfrnascimento@gmail.com>
2015-06-21 01:34:06 -0700

    nginx example

--- ca/sign.sh
+++ ca/sign.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-# Generate cert
-#openssl req -x509 -config openssl-ca.cnf -newkey rsa:4096 -sha256 -nodes -out cacert.pem -outform PEM
-
 #client side
-#KEY_NAME='server'
-#/usr/bin/openssl req  -new -newkey rsa:4096 -days 365 -nodes -keyout ${KEY_NAME}.key -out ${KEY_NAME}.csr
+KEY_NAME='server'
+/usr/bin/openssl req  -new -newkey rsa:4096 -days 365 -nodes -keyout ${KEY_NAME}.key -out ${KEY_NAME}.csr
 
 touch index.txt
 echo '01' > serial.txt
