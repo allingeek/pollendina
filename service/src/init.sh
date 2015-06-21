2e96c1850d44a3872e36aa21ebb9ce23512db8b3
huddlesj <huddlesj@gmail.com>
2015-06-21 03:04:29 -0700

    Updated init.sh
    Fixed merge errors.

--- service/src/init.sh
+++ service/src/init.sh
@@ -6,30 +6,27 @@
 #   index.txt
 #   serial.txt
 
+#Files to copy in
 FILES=(openssl-ca.cnf cacert.pem cakey.pem index.txt serial.txt)
+#Source directory
 SDIR="/pollendina"
+#Destination directory
 DDIR="/opt/pollendina"
 
 for i in ${FILES[@]}; do
   # Test existence of each of the require resources
   if [ ! -f $DDIR/$i ];then
-          cp $SDIR/$i $DDIR/$i
+     cp $SDIR/$i $DDIR/$i
   fi
 done
 
-<<<<<<< HEAD
-=======
- touch /opt/pollendina/index.txt
+# touch /opt/pollendina/index.txt
 
-echo 01 >> /opt/pollendina/serial.txt
->>>>>>> origin/master
+#echo 01 >> /opt/pollendina/serial.txt
 
- openssl req -x509 -config openssl-ca.cnf -newkey rsa:4096 -sha256 -nodes -out cacert.pem -outform PEM
+# openssl req -x509 -config openssl-ca.cnf -newkey rsa:4096 -sha256 -nodes -out cacert.pem -outform PEM
 
-<<<<<<< HEAD
-=======
- cp /pollendina/openssl-ca.cnf /opt/pollendina/openssl-ca.cnf
->>>>>>> origin/master
+# cp /pollendina/openssl-ca.cnf /opt/pollendina/openssl-ca.cnf
 
 
 exec "$@" # run the default command
