4751f48fdc254688bbfeab485fd131d4442972ee
Jeff Nickoloff <jeff@allingeek.com>
2015-06-25 00:08:55 -0700

    Moved mux code into its own file.
    Signed-off-by: Jeff Nickoloff <jeff@allingeek.com>

---
+++ service/src/mux.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+	"regexp"
+)
+
+type MuxRoute struct {
+        pattern *regexp.Regexp
+        handler http.Handler
+}
+
+type RegexHandler struct {
+        rs []*MuxRoute
+}
+
+func (rh *RegexHandler) Handler(p *regexp.Regexp, h http.Handler) {
+        rh.rs = append(rh.rs, &MuxRoute{p, h})
+}
+
+func (rh *RegexHandler) HandleFunc(p *regexp.Regexp, h func(http.ResponseWriter, *http.Request)) {
+        rh.rs = append(rh.rs, &MuxRoute{p, http.HandlerFunc(h)})
+}
+
+func (rh *RegexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+        for _, route := range rh.rs {
+                if route.pattern.MatchString(r.URL.Path) {
+                        route.handler.ServeHTTP(w, r)
+                        return
+                }
+        }
+        Warning.Printf("Route not found: %s", r.URL.Path)
+        http.NotFound(w, r)
+}
